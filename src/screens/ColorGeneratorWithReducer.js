dc0cf68ff5e90d5c1f77b6a577ad704e37dd061d
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-15 17:58:32 -0500

    validated colorgenerator with reducer screen

--- src/screens/ColorGeneratorWithReducer.js
+++ src/screens/ColorGeneratorWithReducer.js
@@ -1,14 +1,24 @@
 import React, { useReducer } from "react";
-import { Button, StyleSheet, View, Text } from "react-native";
+import { Button, StyleSheet, View, Text, ActionSheetIOS } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 const reducer = (state, action)=>{
     switch(action.colorToChange){
         case 'red':
-            return { ...state, red: state.red + action.amount }
+            return state.red + action.amount > 255 ||
+              state.red + action.amount < 0
+              ? state
+              : { ...state, red: state.red + action.amount };
         case 'green':
-            return { ...state, green: state.green + action.amount }
+            return state.red + action.amount > 255 ||
+              state.red + action.amount < 0
+              ? state
+              : { ...state, green: state.green + action.amount };
         case 'blue':
-            return { ...state, blue: state.blue + action.amount }
+            return state.red + action.amount > 255 ||
+            state.red + action.amount < 0
+            ? state
+            : { ...state, blue: state.blue + action.amount };
+
         default:
             return state;
     }
